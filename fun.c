0175a635ad69492ed9b5a75c941efef7ba987074
Yakir Dorani <crazzywak@gmail.com>
2019-08-06 15:07:31 +0300

    Backup

---
+++ fun.c
@@ -0,0 +1,91 @@
+#include <linux/init.h>
+#include <linux/module.h>
+#include <linux/kernel.h>
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("Robert W. Oliver II");
+MODULE_DESCRIPTION("A simple example Linux module.");
+MODULE_VERSION("0.01");
+
+//#define IDENTITY_NAME_LEN 15
+
+//typedef int bool;
+//enum { false, true };
+
+typedef struct identity {
+//    char name[IDENTITY_NAME_LEN];
+    int id;
+//	bool busy;
+} identity;
+
+typedef struct node {
+    identity obj;
+    struct node *next;
+} node_t;
+
+
+static node_t *head = NULL;
+
+//static void identity_create(char str[20] name, int id) {
+//	push(
+//}
+
+static void identity_create(int id) {
+    node_t *cur = head;
+
+	printk(KERN_INFO "Creating identity");
+    if (head == NULL) {
+	printk(KERN_INFO "Allocating memory");
+        head = vmalloc(sizeof(node_t));
+	printk(KERN_INFO "Memory allocated");
+        head->obj.id = id;
+        head->next = NULL;
+        return;
+    }
+
+
+    while (cur->next != NULL) {
+        cur = cur->next;
+    }
+
+    /* now we can add a new variable */
+    cur->next = vmalloc(sizeof(node_t));
+    cur->next->obj.id = id;
+    cur->next->next = NULL;
+}
+
+static void identity_destroy(int id) {
+    node_t *cur = head;
+
+	printk(KERN_INFO "Destroying identity");
+
+    while (cur != NULL) {
+	node_t *next = cur->next;
+	if (cur->obj.id == id) {
+	printk(KERN_INFO "Freeing memory!");
+		vfree(cur);
+	printk(KERN_INFO "Memory freed");
+		continue;
+	}
+
+        cur = next;
+    }
+}
+
+static int __init lkm_example_init(void) {
+
+	identity_create(1);
+
+	identity_destroy(1);
+
+	printk(KERN_INFO "Fun init!\n");
+
+        return 0;
+}
+
+static void __exit lkm_example_exit(void) {
+ printk(KERN_INFO "Fun, exit!\n");
+}
+
+module_init(lkm_example_init);
+module_exit(lkm_example_exit);
