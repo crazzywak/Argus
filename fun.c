729639a3a0f2c89d95ad49a523a0de971b69ade2
Yakir Dorani <crazzywak@gmail.com>
2019-08-08 07:09:08 +0300

    Backup

--- fun.c
+++ fun.c
@@ -20,6 +20,7 @@ typedef struct identity {
 
 typedef struct node {
     identity obj;
+    struct node *previous;
     struct node *next;
 } node_t;
 
@@ -32,6 +33,7 @@ static node_t *head = NULL;
 
 static void identity_create(int id) {
     node_t *cur = head;
+    node_t *prev = NULL;
 
     if (head == NULL) {
 	printk(KERN_INFO "Creating first identity: %d", id);
@@ -40,48 +42,61 @@ static void identity_create(int id) {
 	printk(KERN_INFO "Memory allocated!");
         head->obj.id = id;
 	head->obj.busy = false;
+	head->previous = NULL;
         head->next = NULL;
         return;
     }
 
     while (cur->next != NULL) {
-        cur = cur->next;
+	prev = cur;
+	cur = cur->next;
     }
 
 	printk(KERN_INFO "Creating identity: %d", id);
 
     /* now we can add a new variable */
 	printk(KERN_INFO "Allocating memory...");
-    cur->next = vmalloc(sizeof(node_t));
+	cur->next = vmalloc(sizeof(node_t));
 	printk(KERN_INFO "Memory allocated!");
-    cur->next->obj.id = id;
-    cur->next->next = NULL;
+	cur->previous = prev;
+	cur->next->obj.id = id;
+	cur->next->obj.busy = false;
+	cur->next->next = NULL;
+	cur->next->previous = cur;
 }
 
-static void identity_destroy(int id) {
+static node_t * identity_find(int id) {
     node_t *cur = head;
-
-    printk(KERN_INFO "Destroying identity: %d", id);
-
-    node_t *previous = NULL;
+    
+    printk(KERN_INFO "Finding identity: %d", id);
 
     while (cur != NULL) {
 	node_t *next = cur->next;
 	if (cur->obj.id == id) {
-		printk(KERN_INFO "Freeing memory!");
-		vfree(cur);
-		printk(KERN_INFO "Memory freed");
-		
-		if (previous != NULL) {
-			previous->next=next;
-		}
-
-		break;
+		printk(KERN_INFO "Found identity! %d", id);
+		return cur;
 	}
 
-	previous = cur;
         cur = next;
     }
+}
+
+static void identity_destroy(int id) {
+	node_t *n = identity_find(id);
+
+	printk(KERN_INFO "Destroying identity: %d", id);
+
+	if (n != NULL) {
+		if (n->previous == NULL) {
+			head = n->next;
+		} else {
+			n->previous->next = n->next;
+			n->next->previous = n->previous;
+		}
+
+		vfree(n);
+		printk(KERN_INFO "Memory freed");
+	}
 }
 
 static int __init lkm_example_init(void) {
